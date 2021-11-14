245966b6a23256832ffa601febbe4bec8f0706a3
Matthew Gordon <matthew@gordon.earth>
2021-11-14 14:27:27 -0500

    Flesh out database schema

--- api/src/db.rs
+++ api/src/db.rs
@@ -1,5 +1,5 @@
-use rocket::{Rocket, Build};
 use rocket::fairing::AdHoc;
+use rocket::{Build, Rocket};
 use rocket_sync_db_pools::postgres;
 
 #[database("postgres_db")]
@@ -10,14 +10,25 @@ async fn init_db(rocket: Rocket<Build>) -> Rocket<Build> {
         .await
         .expect("database mounted")
         .run(|conn| {
-            conn.execute(
-                "CREATE TABLE registrations (
+            conn.batch_execute(
+                "CREATE TABLE IF NOT EXISTS attendees (
                     id SERIAL PRIMARY KEY,
                     name TEXT NOT NULL,
                     email TEXT,
                     phone TEXT NOT NULL
-                )",
-                &[],
+                );
+
+                CREATE TABLE IF NOT EXISTS events (
+                    id SERIAL PRIMARY KEY,
+                    name TEXT NOT NULL,
+                    time TIMESTAMP NOT NULL
+                );
+
+                CREATE TABLE IF NOT EXISTS registrations (
+                    attendee_id INTEGER REFERENCES attendees (id),
+                    event_id INTEGER REFERENCES events (id),
+                    PRIMARY KEY(attendee_id, event_id)
+                );",
             )
         })
         .await
