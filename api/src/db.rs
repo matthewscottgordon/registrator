4a234dae8be794cebbe7107a9b6f56f91af8b48f
Matthew Gordon <matthew@gordon.earth>
2021-11-14 20:39:49 -0500

    Add /events/add to API

--- api/src/db.rs
+++ api/src/db.rs
@@ -1,9 +1,25 @@
+use postgres::error::Error;
 use rocket::fairing::AdHoc;
+use rocket::serde::json::Json;
 use rocket::{Build, Rocket};
 use rocket_sync_db_pools::postgres;
 
+use super::Event;
+
 #[database("postgres_db")]
-struct Db(postgres::Client);
+pub struct Db(postgres::Client);
+
+impl Db {
+    pub async fn add_event<'a>(self, event: Json<Event>) -> Result<u64, Error> {
+        self.run(move |conn| {
+            conn.execute(
+                "INSERT INTO events (name, time) VALUES ($1, $2);",
+                &[&event.name, &event.datetime],
+            )
+        })
+        .await
+    }
+}
 
 async fn init_db(rocket: Rocket<Build>) -> Rocket<Build> {
     Db::get_one(&rocket)
@@ -21,7 +37,7 @@ async fn init_db(rocket: Rocket<Build>) -> Rocket<Build> {
                 CREATE TABLE IF NOT EXISTS events (
                     id SERIAL PRIMARY KEY,
                     name TEXT NOT NULL,
-                    time TIMESTAMP NOT NULL
+                    time TIMESTAMP WITH TIME ZONE NOT NULL
                 );
 
                 CREATE TABLE IF NOT EXISTS registrations (
--- api/src/main.rs
+++ api/src/main.rs
@@ -1,16 +1,51 @@
-#[macro_use] extern crate rocket;
-#[macro_use] extern crate rocket_sync_db_pools;
+#[macro_use]
+extern crate rocket;
+#[macro_use]
+extern crate rocket_sync_db_pools;
+
+use chrono::{DateTime, Utc};
+use rocket::response::Debug;
+use rocket::serde::{de, json::Json, Deserialize, Deserializer};
+use std::fmt::Display;
+use std::str::FromStr;
 
 mod db;
+
+fn deserialize_from_str<'de, S, D>(deserializer: D) -> Result<S, D::Error>
+where
+    S: FromStr,      // Required for S::from_str...
+    S::Err: Display, // Required for .map_err(de::Error::custom)
+    D: Deserializer<'de>,
+{
+    let s: String = Deserialize::deserialize(deserializer)?;
+    S::from_str(&s).map_err(de::Error::custom)
+}
 
 #[get("/")]
 fn index() -> &'static str {
     "Hello, world!"
 }
 
+#[derive(Deserialize)]
+#[serde(crate = "rocket::serde")]
+pub struct Event {
+    name: String,
+    #[serde(deserialize_with = "deserialize_from_str")]
+    datetime: DateTime<Utc>,
+}
+
+#[post("/events/add", data = "<event>")]
+async fn add_event(
+    db: db::Db,
+    event: Json<Event>,
+) -> Result<Json<u64>, Debug<postgres::error::Error>> {
+    let count = db.add_event(event).await?;
+    Ok(Json(count))
+}
+
 #[launch]
 fn rocket() -> _ {
     rocket::build()
         .attach(db::stage())
-        .mount("/", routes![index])
+        .mount("/", routes![index, add_event])
 }
